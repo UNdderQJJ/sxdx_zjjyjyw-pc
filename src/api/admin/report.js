34e5ce922b2b64118254fc35d0cf52b182ca096a
UNdder <3143028662@qq.com>
2024-04-07 15:26:53 +0800

    工单报表查询重写

--- src/api/admin/report.js
+++ src/api/admin/report.js
@@ -3,42 +3,9 @@ import request from '@/utils/request'
 // 查询工单报表列表
 export function listReport(query) {
   return request({
-    url: '/admin/info/list',
+    url: '/admin/report/list',
     method: 'get',
     params: query
   })
 }
 
-// 查询工单报表详细
-export function getReport(orderId) {
-  return request({
-    url: '/admin/info/' + orderId,
-    method: 'get'
-  })
-}
-
-// 新增工单报表
-export function addReport(data) {
-  return request({
-    url: '/admin/info',
-    method: 'post',
-    data: data
-  })
-}
-
-// 修改工单报表
-export function updateReport(data) {
-  return request({
-    url: '/admin/info',
-    method: 'put',
-    data: data
-  })
-}
-
-// 删除工单报表
-export function delReport(orderId) {
-  return request({
-    url: '/admin/info/' + orderId,
-    method: 'delete'
-  })
-}
